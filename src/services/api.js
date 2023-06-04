4eaca8dda8412170031e3182afb3ff8d626d0013
Maurício <72310791+Mauriciosouza8989@users.noreply.github.com>
2023-06-04 15:00:52 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://rocketnotes-api-1pi1.onrender.com'
+    baseURL: 'https://rocketnotes-api-fmuu.onrender.com'
 });
