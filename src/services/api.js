d657d256025a7650ebe29e62fbff9783ffb92a3f
Maurício <72310791+Mauriciosouza8989@users.noreply.github.com>
2024-01-13 15:45:03 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-<<<<<<< HEAD
     baseURL: 'https://noteapi-0tjp.onrender.com'
 });
-=======
-    baseURL: 'https://notes-api-zeeh.onrender.com'
-});
->>>>>>> 40d17b52835dce49da98405aae17fc60d653f3e8
+
